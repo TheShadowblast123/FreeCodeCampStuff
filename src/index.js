034fc21cb8107b3e9345b5661db35a96eeea94b1
Shadow <TheShadowblast123@yahoo.com>
2023-08-14 16:06:12 -0700

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "../_snowpack/pkg/react.js";
-import ReactDOM from "../_snowpack/pkg/react-dom.js";
-import App from "./App.js";
-import "./index.css.proxy.js";
-ReactDOM.render(/* @__PURE__ */ React.createElement(App, {
-  page: "index"
-}), document.getElementById("root"));
